f42429af40b157d0c3f482006aaba89ef401adfe
Anton Volkov <anton@wolkow.ru>
2022-11-01 17:55:00 +0200

    change size

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,8 @@
 export const MAPBOX_API_KEY =
+  process.env.MAPBOX_API_KEY ||
   "pk.eyJ1IjoiaG9sbC1wYXBhemFpbiIsImEiOiJjbDl0azdlMDUwZmNhM25teTRmeGZtYzU0In0.Ux5rEeE_cpf0iVmGR8g2Ug";
 export const MAPBOX_STYLE =
+  process.env.MAPBOX_STYLE ||
   "mapbox://styles/holl-papazain/cl9tzj20f00aj14p0ewojow32";
 
 export const INTERVAL_IN_MS = Number(process.env.INTERVAL_IN_MS) || 50;
